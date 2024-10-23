9aba00b426b388fd8f7c430f14e0cef73e49b1d4
notnagol3 <140694316+MearlyAMortal@users.noreply.github.com>
2024-10-22 21:50:48 -0700

    Symbol table linked list progress
    Barebones linked list set up with basic functions. Should work, still compiles, havent tested functionality yet.

--- symbols.h
+++ symbols.h
@@ -1,5 +1,6 @@
 //
 // Created by Ethan Edwards on 10/11/2024.
+// Edited by Logan Puntous on 10/22/2024.
 //
 
 #ifndef CPLANE_SYMBOLS_H
@@ -11,80 +12,36 @@
 class Symbol {
 public:
   int data;
-  Symbol* next;
+  Symbol *next;
 
   // Default constructor
-  Symbol() {
-    std::string IDENT_NAME;                                                                                                                                         
-    int IDENT_TYPE;                                                                                                                                                 
-    int DATATYPE = -1;                                                                                                                                              
-    bool IS_ARRAY = false;                                                                                                                                           
-    int ARRAY_SIZE = 0;                                                                                                                                              
-    int SCOPE = 1;
-
-    next = NULL;
-  }
-
+  Symbol();
+  
   // Constructor
-  Symbol(std::string IDENT_NAME, int IDENT_TYPE, int DATATYPE, bool IS_ARRAY, int ARRAY_SIZE, int SCOPE) {
-    this->IDENT_NAME = IDENT_NAME;
-    this->IDENT_TYPE = IDENT_TYPE;
-    this->DATATYPE = DATATYPE;
-    this->IS_ARRAY = IS_ARRAY;
-    this->ARRAY_SIZE = ARRAY_SIZE;
-    this->SCOPE = SCOPE;
-
-    this->next = NULL;
-  }
+  Symbol(std::string IDENT_NAME, int IDENT_TYPE, int DATATYPE, bool IS_ARRAY, int ARRAY_SIZE, int SCOPE);
 };
+
+
+
+
+
+
+
 
 class SymbolTable {
-  Symbol *temp = head;
+  Symbol *head;
   
 public:
-  Symbols() {
+  // Constructor
+  SymbolTable() {
     head = NULL;
   }
-
-  void insertAtHead(std::string IDENT_NAME, int IDENT_TYPE, int DATATYPE, bool IS_ARRAY, int ARRAY_SIZE, int SCOPE) {
-
-    // Create the new Node
-    Symbol *newSymbol = new Symbol(IDENT_NAME, IDENT_TYPE, DATATYPE, IS_ARRAY, ARRAY_SIZE, SCOPE);
-
-    // First
-    if (head == NULL) {
-        head = newSymbol;
-	return;
-    }
-
-    // Insert the newly created linked list at the head
-    newSymbol->next = this->head;
-    this->head = newSymbol;
-  }
   
+  ~SymbolTable();
+  
+  void insertAtHead(std::string IDENT_NAME, int IDENT_TYPE, int DATATYPE, bool IS_ARRAY, int ARRAY_SIZE, int SCOPE);
+  void print();
 };
 
-
-
-/*
-class symbol {
-public:
-    symbol() = default;
-    ~symbol() = default;
-
-private:
-    std::string IDENT_NAME;
-    int IDENT_TYPE;
-    int DATATYPE = -1;
-    bool IS_ARRAY = false;
-    int ARRAY_SIZE = 0;
-    int SCOPE = 1;
-
-};
-
-class symbols {
-
-};
-*/
 
 #endif //CPLANE_SYMBOLS_H
