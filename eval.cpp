66441ad6d2b6b007ee55555d91b146de9c0a79c6
Ethan Edwards <evanevansonbusiness@gmail.com>
2024-11-14 13:42:53 -0800

    Finished defs for basic eval functions

--- eval.cpp
+++ eval.cpp
@@ -3,3 +3,169 @@
 //
 
 #include "eval.h"
+
+template <typename T, typename U> // +
+auto eval::add(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+        (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, std::string>::value && std::is_same<U, std::string>::value) ||
+        (std::is_same<T, std::string>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, int>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, std::string>::value), decltype(a + b)>::type {
+    return a + b;
+}
+
+template <typename T, typename U> // -
+auto eval::sub(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+        (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, int>::value), decltype(a - b)>::type {
+    return a - b;
+}
+
+template <typename T, typename U> // *
+auto eval::mul(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+        (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, int>::value), decltype(a * b)>::type {
+    return a * b;
+}
+
+template <typename T, typename U> // /
+auto eval::div(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+        (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, int>::value), decltype(a / b)>::type {
+    return a / b;
+}
+
+template <typename T, typename U> // %
+auto eval::mod(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+        (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, int>::value), decltype(a % b)>::type {
+    return a % b;
+}
+
+template <typename T, typename U> // >
+auto eval::grt(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+        (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, std::string>::value && std::is_same<U, std::string>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, int>::value), decltype(a > b)>::type {
+    return a > b;
+}
+
+template <typename T, typename U> // <
+auto eval::lst(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+        (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, std::string>::value && std::is_same<U, std::string>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, int>::value), decltype(a < b)>::type {
+    return a < b;
+}
+
+template<typename T> // !
+auto eval::op_not(T a) -> typename std::enable_if<std::is_same<T, bool>::value, decltype(!a)>::type {
+    return !a;
+}
+
+template<typename T> // ++
+auto eval::inc(T a) -> typename std::enable_if<std::is_arithmetic<T>::value, decltype(a + 1)>::type {
+    return a + 1;
+}
+
+template<typename T> // --
+auto eval::dec(T a) -> typename std::enable_if<std::is_arithmetic<T>::value, decltype(a - 1)>::type {
+    return a - 1;
+}
+
+template<typename T, typename U> // +=
+auto eval::add_equ(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+        (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, std::string>::value && std::is_same<U, std::string>::value) ||
+        (std::is_same<T, std::string>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, std::string>::value && std::is_same<U, int>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, int>::value), decltype(a + b)>::type {
+    return a + b;
+}
+
+template<typename T, typename U> // -=
+auto eval::sub_equ(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+        (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, int>::value), decltype(a - b)>::type {
+    return a - b;
+}
+
+template<typename T, typename U> // *=
+auto eval::mul_equ(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+        (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, int>::value), decltype(a * b)>::type {
+    return a * b;
+}
+
+template<typename T, typename U> // /=
+auto eval::div_equ(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+        (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, int>::value), decltype(a / b)>::type {
+    return a / b;
+}
+
+template<typename T, typename U> // %=
+auto eval::mod_equ(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+        (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, int>::value), decltype(a % b)>::type {
+    return a % b;
+}
+
+template<typename T, typename U> // ==
+auto eval::equ(T a, U b) -> typename std::enable_if<std::is_same<T, U>::value, decltype(a == b)>::type {
+    return a == b;
+}
+
+template<typename T, typename U> // !=
+auto eval::not_equ(T a, U b) -> typename std::enable_if<std::is_same<T, U>::value, decltype(a != b)>::type {
+    return a != b;
+}
+
+template<typename T, typename U> // >=
+auto eval::grt_equ(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+        (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, std::string>::value && std::is_same<U, std::string>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, int>::value), decltype(a >= b)>::type {
+    return a >= b;
+}
+
+template<typename T, typename U> // <=
+auto eval::lst_equ(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+        (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, std::string>::value && std::is_same<U, std::string>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, int>::value), decltype(a <= b)>::type {
+    return a <= b;
+}
+
+template<typename T, typename U> // &&
+auto eval::and_op(T a, U b) -> typename std::enable_if<(std::is_same<T, bool>::value && std::is_same<U, bool>::value) ||
+        (std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+        (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, int>::value), decltype(a && b)>::type {
+    return a && b;
+}
+
+template<typename T, typename U> // ||
+auto eval::or_op(T a, U b) -> typename std::enable_if<(std::is_same<T, bool>::value && std::is_same<U, bool>::value) ||
+        (std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+        (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+        (std::is_same<T, char>::value && std::is_same<U, int>::value), decltype(a || b)>::type {
+    return a || b;
+}
+
--- eval.h
+++ eval.h
@@ -6,55 +6,134 @@
 #define CPLANE_EVAL_H
 
 #include <type_traits>
+#include <string>
 
 class eval {
 public:
-    // TODO: GO THROUGH THIS PILE OF GARBAGE AND ADD ALL VALID DATATYPE COMBOS :vomit:
+    eval() = default;
+
     template <typename T, typename U> // +
-    auto add(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value), decltype(a + b)>::type;
+    auto add(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+            (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, std::string>::value && std::is_same<U, std::string>::value) ||
+            (std::is_same<T, std::string>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, int>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, std::string>::value), decltype(a + b)>::type;
+
     template <typename T, typename U> // -
-    auto sub(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value), decltype(a - b)>::type;
+    auto sub(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+            (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, int>::value), decltype(a - b)>::type;
+
     template <typename T, typename U> // *
-    auto mul(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value), decltype(a * b)>::type;
+    auto mul(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+            (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, int>::value), decltype(a * b)>::type;
+
     template <typename T, typename U> // /
-    auto div(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value), decltype(a / b)>::type;
+    auto div(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+            (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, int>::value), decltype(a / b)>::type;
+
     template <typename T, typename U> // %
-    auto mod(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value), decltype(a % b)>::type;
+    auto mod(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+            (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, int>::value), decltype(a % b)>::type;
     // = not handled here
     template <typename T, typename U> // >
-    auto grt(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value), decltype(a > b)>::type;
+    auto grt(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+            (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, std::string>::value && std::is_same<U, std::string>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, int>::value), decltype(a > b)>::type;
+
     template <typename T, typename U> // <
-    auto lst(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value), decltype(a < b)>::type;
+    auto lst(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+            (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, std::string>::value && std::is_same<U, std::string>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, int>::value), decltype(a < b)>::type;
+
     template<typename T> // !
     auto op_not(T a) -> typename std::enable_if<std::is_same<T, bool>::value, decltype(!a)>::type;
     // TODO: ADD BITWISE OPERATORS (AND [&], OR [|], XOR [^], NOT [~], SHIFT [<<, >>])
     template<typename T> // ++
     auto inc(T a) -> typename std::enable_if<std::is_arithmetic<T>::value, decltype(a + 1)>::type;
+
     template<typename T> // --
     auto dec(T a) -> typename std::enable_if<std::is_arithmetic<T>::value, decltype(a - 1)>::type;
+
     template<typename T, typename U> // +=
-    auto add_equ(T a, U b) -> typename std::enable_if<std::is_arithmetic<T>::value && std::is_arithmetic<U>::value, decltype(a + b)>::type;
+    auto add_equ(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+            (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, std::string>::value && std::is_same<U, std::string>::value) ||
+            (std::is_same<T, std::string>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, std::string>::value && std::is_same<U, int>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, int>::value), decltype(a + b)>::type;
+
     template<typename T, typename U> // -=
-    auto sub_equ(T a, U b) -> typename std::enable_if<std::is_arithmetic<T>::value && std::is_arithmetic<U>::value, decltype(a - b)>::type;
+    auto sub_equ(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+            (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, int>::value), decltype(a - b)>::type;
+
     template<typename T, typename U> // *=
-    auto mul_equ(T a, U b) -> typename std::enable_if<std::is_arithmetic<T>::value && std::is_arithmetic<U>::value, decltype(a * b)>::type;
+    auto mul_equ(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+            (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, int>::value), decltype(a * b)>::type;
+
     template<typename T, typename U> // /=
-    auto div_equ(T a, U b) -> typename std::enable_if<std::is_arithmetic<T>::value && std::is_arithmetic<U>::value, decltype(a / b)>::type;
+    auto div_equ(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+            (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, int>::value), decltype(a / b)>::type;
+
     template<typename T, typename U> // %=
-    auto mod_equ(T a, U b) -> typename std::enable_if<std::is_arithmetic<T>::value && std::is_arithmetic<U>::value, decltype(a % b)>::type;
+    auto mod_equ(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+            (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, int>::value), decltype(a % b)>::type;
     // TODO: ADD LSHIFT_EQUALS AND RSHIFT_EQUALS
     template<typename T, typename U> // ==
     auto equ(T a, U b) -> typename std::enable_if<std::is_same<T, U>::value, decltype(a == b)>::type;
+
     template<typename T, typename U> // !=
     auto not_equ(T a, U b) -> typename std::enable_if<std::is_same<T, U>::value, decltype(a != b)>::type;
+
     template<typename T, typename U> // >=
-    auto grt_equ(T a, U b) -> typename std::enable_if<std::is_arithmetic<T>::value && std::is_arithmetic<U>::value, decltype(a >= b)>::type;
+    auto grt_equ(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+            (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, std::string>::value && std::is_same<U, std::string>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, int>::value), decltype(a >= b)>::type;
+
     template<typename T, typename U> // <=
-    auto lst_equ(T a, U b) -> typename std::enable_if<std::is_arithmetic<T>::value && std::is_arithmetic<U>::value, decltype(a <= b)>::type;
+    auto lst_equ(T a, U b) -> typename std::enable_if<(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+            (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, std::string>::value && std::is_same<U, std::string>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, int>::value), decltype(a <= b)>::type;
+
     template<typename T, typename U> // &&
-    auto and_op(T a, U b) -> typename std::enable_if<(std::is_same<T, bool>::value && std::is_same<U, bool>::value), decltype(a && b)>::type;
+    auto and_op(T a, U b) -> typename std::enable_if<(std::is_same<T, bool>::value && std::is_same<U, bool>::value) ||
+            (std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+            (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, int>::value), decltype(a && b)>::type;
+
     template<typename T, typename U> // ||
-    auto or_op(T a, U b) -> typename std::enable_if<(std::is_same<T, bool>::value && std::is_same<U, bool>::value), decltype(a || b)>::type;
+    auto or_op(T a, U b) -> typename std::enable_if<(std::is_same<T, bool>::value && std::is_same<U, bool>::value) ||
+            (std::is_arithmetic<T>::value && std::is_arithmetic<U>::value) ||
+            (std::is_same<T, int>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, char>::value) ||
+            (std::is_same<T, char>::value && std::is_same<U, int>::value), decltype(a || b)>::type;
 };
 
 
